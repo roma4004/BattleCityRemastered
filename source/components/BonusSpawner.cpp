b3f6971e07cb253b6af38cf90f109937381bef86
Dentair <dentair4004@gmail.com>
2026-06-29 11:44:27 +0300

    fix reseting bonus spawner time on new game mode

--- source/components/BonusSpawner.cpp
+++ source/components/BonusSpawner.cpp
@@ -37,6 +37,7 @@ BonusSpawner::~BonusSpawner() { Unsubscribe(); }
 
 void BonusSpawner::Subscribe()
 {
+	_events->AddListener("Reset", _name, [this]() { this->Reset(); });
 	_events->AddListener("GameModeChangedTo", _name, [this](const GameMode newGameMode)
 	{
 		this->_gameMode = newGameMode;
@@ -152,4 +153,9 @@ void BonusSpawner::SpawnRandomBonus(const ObjRectangle rect)
 {
 	const auto bonusType = static_cast<BonusType>(RandUtils::GetRandNumber(_distSpawnType));
 	SpawnBonus(rect, bonusType);
+}
+
+void BonusSpawner::Reset()
+{
+	_lastTimeSpawn = std::chrono::system_clock::now();
 }
